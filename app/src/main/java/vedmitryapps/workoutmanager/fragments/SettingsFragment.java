2fe1b12482da5bdfc1f688f4161b2758d8ebf0f6
vedmitry7 <vedmitry7@gmail.com>
2019-02-26 23:23:43 +0300

    add black theme

--- app/src/main/java/vedmitryapps/workoutmanager/fragments/SettingsFragment.java
+++ app/src/main/java/vedmitryapps/workoutmanager/fragments/SettingsFragment.java
@@ -2,6 +2,7 @@ package vedmitryapps.workoutmanager.fragments;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -19,10 +20,13 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 
+import org.greenrobot.eventbus.EventBus;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import vedmitryapps.workoutmanager.Constants;
+import vedmitryapps.workoutmanager.Events;
 import vedmitryapps.workoutmanager.R;
 import vedmitryapps.workoutmanager.SharedManager;
 import vedmitryapps.workoutmanager.adapters.SoundRecyclerAdapter;
@@ -30,22 +34,19 @@ import vedmitryapps.workoutmanager.adapters.SoundRecyclerAdapter;
 
 public class SettingsFragment extends Fragment {
 
-    @BindView(R.id.switchVibration)
-    Switch switchVibration;
-
-    @BindView(R.id.switchNotification)
-    Switch switchNotification;
-
-    @BindView(R.id.switchNotificationTime)
-    Switch switchNotificationTime;
-
-    @BindView(R.id.defaultSoundContainer)
-    ConstraintLayout sound;
-
-    @BindView(R.id.soundName)
-    TextView soundName;
-
-
+    @BindView(R.id.switchVibration) Switch switchVibration;
+    @BindView(R.id.switchNotification) Switch switchNotification;
+    @BindView(R.id.switchNotificationTime) Switch switchNotificationTime;
+    @BindView(R.id.switchBlack) Switch switchBlack;
+    @BindView(R.id.defaultSoundContainer) ConstraintLayout sound;
+    @BindView(R.id.toolbarLayout) ConstraintLayout toolbar;
+    @BindView(R.id.mainContainer) ConstraintLayout mainContainer;
+    @BindView(R.id.soundName) TextView soundName;
+    @BindView(R.id.textView) TextView textView;
+    @BindView(R.id.textView2) TextView textView2;
+    @BindView(R.id.textView3) TextView textView3;
+    @BindView(R.id.textView4) TextView textView4;
+    @BindView(R.id.textView5) TextView textView5;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -57,13 +58,14 @@ public class SettingsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        initColors();
         switchVibration.setChecked(SharedManager.getProperty(Constants.KEY_DEF_VIBRATION));
         switchVibration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 SharedManager.addProperty(Constants.KEY_DEF_VIBRATION, isChecked);
             }
         });
-
 
         switchNotification.setChecked(!SharedManager.getProperty(Constants.KEY_NOTIFICATION_DISABLED));
         switchNotification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -77,6 +79,38 @@ public class SettingsFragment extends Fragment {
                 SharedManager.addProperty(Constants.KEY_NOTIFICATION_TIME_DISABLED, !isChecked);
             }
         });
+
+        switchBlack.setChecked(SharedManager.getProperty(Constants.KEY_BLACK_ENABLED));
+        switchBlack.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedManager.addProperty(Constants.KEY_BLACK_ENABLED, isChecked);
+                initColors();
+            }
+        });
+    }
+
+    private void initColors() {
+        EventBus.getDefault().post(new Events.SetStatusBar());
+        if(SharedManager.getProperty(Constants.KEY_BLACK_ENABLED)){
+            int color = getContext().getResources().getColor(R.color.white_95);
+            toolbar.setBackgroundColor(getResources().getColor(R.color.colorToolbar));
+            mainContainer.setBackgroundColor(getResources().getColor(R.color.colorBackground));
+            textView.setTextColor(color);
+            textView2.setTextColor(color);
+            textView3.setTextColor(color);
+            textView4.setTextColor(color);
+            textView5.setTextColor(color);
+            soundName.setTextColor(color);
+        } else {
+            toolbar.setBackgroundColor(getResources().getColor(R.color.toolbar_new));
+            mainContainer.setBackgroundColor(getResources().getColor(R.color.background_new));
+            textView.setTextColor(Color.BLACK);
+            textView2.setTextColor(Color.BLACK);
+            textView3.setTextColor(Color.BLACK);
+            textView4.setTextColor(Color.BLACK);
+            textView5.setTextColor(Color.BLACK);
+            soundName.setTextColor(Color.BLACK);
+        }
     }
 
     @OnClick(R.id.defaultSoundContainer)
